f4ddcf36d67f31870577dc3a263023d093737e8d
xjxliuxing <xjxliuxing@163.com>
2022-07-24 11:16:56 +0800

    init

--- app/src/main/java/com/xjx/kotlin/MainActivity.kt
+++ app/src/main/java/com/xjx/kotlin/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.xjx.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.helper.utils.LogUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val intArrayOf = intArrayOf(1, 2, 3)
-        intArrayOf.forEach {  }
+
+        // 遍历集合，不带角标
+        for (item in intArrayOf) {
+            LogUtil.e("-$item")
+        }
     }
 }
