24601add58171876a8b13c0be1686bdd22ec611a
xjxliuxing <xjxliuxing@163.com>
2022-08-07 22:55:13 +0800

    测试

---
+++ app/src/main/java/com/xjx/kotlin/ui/activity/test/Test33.kt
@@ -0,0 +1,28 @@
+package com.xjx.kotlin.ui.activity.test
+
+/**
+ * @author : 流星
+ * @CreateDate: 2022/7/26-7:36
+ * @Description:
+ */
+object Test33 {
+
+    var a = "1"
+    var B: String? = null
+    const val c = "3"
+    const val d = ""
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+
+    public fun test() {
+        val text4 = Text4()
+        text4.test3()
+        Text4.test1()
+        Text4.a
+        Text4.B
+        text4.e
+    }
+}
