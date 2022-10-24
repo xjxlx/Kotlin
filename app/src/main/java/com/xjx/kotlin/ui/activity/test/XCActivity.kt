2f20f97044129843fa7472dc35da2666156d04b2
xjxliuxing <xjxliuxing@163.com>
2022-10-24 22:53:40 +0800

    协程

--- app/src/main/java/com/xjx/kotlin/ui/activity/test/XCActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/test/XCActivity.kt
@@ -25,41 +25,44 @@ class XCActivity : AppBaseBindingTitleActivity<ActivityXcactivityBinding>() {
 
         GlobalScope.launch { // 在后台启动一个新的协程并继续
             delay(1000L) // 非阻塞的等待 1 秒钟（默认时间单位是毫秒）
-            LogUtil.e("World!") // 在延迟后打印输出
+            LogUtil.e("World!1") // 在延迟后打印输出
         }
 
-        LogUtil.e("Hello,") // 协程已在等待时主线程还在继续
+        LogUtil.e("Hello,1") // 协程已在等待时主线程还在继续
         Thread.sleep(2000L) // 阻塞主线程 2 秒钟来保证 JVM 存活
+
+        /**
+         * ---------------------------------------------------
+         */
 
         GlobalScope.launch { // 在后台启动一个新的协程并继续
             delay(1000L)
-            LogUtil.e("World!")
+            LogUtil.e("World2!")
         }
-
-        LogUtil.e("Hello,") // 主线程中的代码会立即执行
+        LogUtil.e("Hello2,") // 主线程中的代码会立即执行
         runBlocking {     // 但是这个表达式阻塞了主线程
             delay(2000L)  // ……我们延迟 2 秒来保证 JVM 的存活
+            LogUtil.e("--=----=.> 2,") // 主线程中的代码会立即执行
         }
 
+        main()
     }
 
     suspend fun test() {
         val job = GlobalScope.launch { // 启动一个新协程并保持对这个作业的引用
             delay(1000L)
-            println("World!")
+            LogUtil.e("World!")
         }
-        println("Hello,")
+        LogUtil.e("Hello,")
         job.join() // 等待直到子协程执行结束
-
-        main()
     }
 
-    fun main() = runBlocking<Unit> { // 开始执行主协程
+    fun main() = runBlocking { // 开始执行主协程
         val launch = GlobalScope.launch { // 在后台启动一个新的协程并继续
             delay(1000L)
-            println("World!")
+            LogUtil.e("World!!!!!")
         }
-        println("Hello,") // 主协程在这里会立即执行
+        LogUtil.e("Hello,!!!!!") // 主协程在这里会立即执行
         delay(2000L)      // 延迟 2 秒来保证 JVM 存活
 
         launch.join()
