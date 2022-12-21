1b93583f5980e001d1ffe58b1f36ba8b1b8a60cc
XJX <xjxliuxing@163.com>
2022-12-21 18:49:07 +0800

    类

--- app/src/main/java/com/xjx/kotlin/ui/activity/test/ClasssActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/test/ClasssActivity.kt
@@ -18,13 +18,14 @@ class ClasssActivity : AppBaseBindingTitleActivity<ActivityClasssBinding>() {
 
         person.show("222")
 
-        // 引用
+        // region 引用
         val kMutableProperty1 = Person::a
         LogUtil.e("" + kMutableProperty1)
         person.let {
             it.a
-
         }
+        //endregion
+
     }
 
     override fun setTitleContent(): String {
@@ -33,5 +34,51 @@ class ClasssActivity : AppBaseBindingTitleActivity<ActivityClasssBinding>() {
 
     override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): ActivityClasssBinding {
         return ActivityClasssBinding.inflate(inflater, container, true)
+    }
+}
+
+class A(var a: Int, val b: String, c: Boolean) {
+    // a 和 b 是全局可见的参数
+    // c 是构造方法和 init块内可见
+}
+
+interface Api {
+    fun a()
+    fun b()
+    fun c()
+}
+
+// 必须实现
+class ImpApi : Api {
+    override fun a() {
+
+    }
+
+    override fun b() {
+    }
+
+    override fun c() {
+    }
+}
+
+// 必须实现
+class ApiWrapper(val api: Api) : Api {
+    override fun a() {
+        api.a()
+    }
+
+    override fun b() {
+        api.b()
+    }
+
+    override fun c() {
+        api.c()
+    }
+}
+
+// 代理实现，让参数 api 去代理对象Api去实现自己需要的事情
+class ApiWrapper2(private val api: Api) : Api by api {
+    override fun a() {
+        api.a()
     }
 }
