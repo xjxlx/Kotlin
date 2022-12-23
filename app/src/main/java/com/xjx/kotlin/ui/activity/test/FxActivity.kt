b00c4c62c9bbac639ac4567f3e8a6ff688c92a9a
XJX <xjxliuxing@163.com>
2022-12-23 17:41:24 +0800

    泛型

--- app/src/main/java/com/xjx/kotlin/ui/activity/test/FxActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/test/FxActivity.kt
@@ -17,5 +17,60 @@ class FxActivity : AppBaseBindingTitleActivity<ActivityFxBinding>() {
     }
 
     override fun initData(savedInstanceState: Bundle?) {
+
+    }
+
+    fun <T : Comparable<T>> test(a: T, b: T): T {
+        val b1 = a > b
+        TODO()
+    }
+
+    fun <T : Int> test2(a: T, b: T): T {
+        val b1 = a > b
+        TODO()
+    }
+
+    class Student<T : Comparable<T>> {
+        fun <R> sss(): R {
+            TODO()
+        }
     }
 }
+
+private val mListOut: ArrayList<out Book> = arrayListOf()
+
+interface Book {
+    fun getBook(): Book?
+}
+
+class BookEnglish : Book {
+    override fun getBook(): Book? {
+//        mListOut.add(BookChina())
+//        val bookStore = BookStore<BookChina>()
+//        val book = bookStore.getBook()
+//        mListOut.add(book)
+
+        return null
+    }
+}
+
+class BookChina : Book {
+    override fun getBook(): Book? {
+        return null
+    }
+}
+
+class BookStore<out T : Book> {
+    fun getBook(): T {
+        TODO()
+    }
+}
+
+class Test2 {
+    fun test2(): Book {
+        val bookStore = BookStore<BookChina>()
+        val book = bookStore.getBook()
+        return book
+    }
+}
+
