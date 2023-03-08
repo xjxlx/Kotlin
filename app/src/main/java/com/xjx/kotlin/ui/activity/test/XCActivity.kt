cc4fdebc6cd5c1e16b14930b88a023c6cf0c0f9e
xjxliuxing <xjxliuxing@163.com>
2023-03-08 19:52:12 +0800

    协程 ---》

--- app/src/main/java/com/xjx/kotlin/ui/activity/test/XCActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/test/XCActivity.kt
@@ -1,17 +1,16 @@
 package com.xjx.kotlin.ui.activity.test
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.android.helper.base.title.AppBaseBindingTitleActivity
 import com.android.helper.utils.LogUtil
 import com.xjx.kotlin.databinding.ActivityXcactivityBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 class XCActivity : AppBaseBindingTitleActivity<ActivityXcactivityBinding>() {
-    private val TAG = "XC"
+    private var TAG = "XC"
 
     override fun setTitleContent(): String {
         return "协程"
@@ -30,17 +29,17 @@ class XCActivity : AppBaseBindingTitleActivity<ActivityXcactivityBinding>() {
 //            delay(1000)
 //            LogUtil.e(TAG, "1: 我是默认线程的 --- 全局的协程！ --- hello")
 //        }
-//
+
 //        // 2：全局协程 --- 指定线程
 //        GlobalScope.launch(Dispatchers.Main) {
 //            LogUtil.e(TAG, "2: 我是指定线程的全局协程！ Thread :" + Thread.currentThread().name)
 //        }
-//
+
 //        // 3：全局协程 --- 指定线程
 //        GlobalScope.launch(Dispatchers.Unconfined) {
 //            LogUtil.e(TAG, "3: 我是指定线程的全局协程！ Thread :" + Thread.currentThread().name)
 //        }
-//
+
 //        // 4: 构建一个阻塞的协程
 //        runBlocking {
 //            LogUtil.e(TAG, "4: 构建阻塞的协程 ！ Thread :" + Thread.currentThread().name)
@@ -48,27 +47,121 @@ class XCActivity : AppBaseBindingTitleActivity<ActivityXcactivityBinding>() {
 //            LogUtil.e(TAG, "4.1 : 阻塞协程的等待")
 //        }
 
-//          main()
+        GlobalScope.launch(Dispatchers.Main) {
+            LogUtil.e("~~~~~~~~~~》》》》》：我是线程：！！！")
+            withContext(Dispatchers.IO) {
+                delay(1000)
+                LogUtil.e("~~~~~~~~~~》》》》》：异步线程：！！！")
+            }
+            LogUtil.e("~~~~~~~~~~》》》》》：我是线程 ---> 结果：！！！")
+        }
 
+        //  GlobalScope.launch(Dispatchers.Main) {
+        //    Log.e("Tag", "xxxxxx1 in " + Thread.currentThread().name)
+        //    doIO()
+        //    Log.e("Tag", "xxxxxx3 in " + Thread.currentThread().name)
+        // }
+
+        // cancel kotlin  coroutines
+        GlobalScope.launch(Dispatchers.Main) {
+            val job = GlobalScope.launch(Dispatchers.Default) {
+                repeat(10) {
+                    LogUtil.e("cancel", "xxxix - $it")
+                    delay(500)
+                }
+            }
+            delay(2000)
+            // job.cancel()
+            LogUtil.e("cancel", "xxxix - job wait")
+            job.join()
+            LogUtil.e("cancel", "xxxix - job finish")
+        }
+
+        // time out or null
+        val jobTimeOutNull = GlobalScope.launch(Dispatchers.Main) {
+            TAG = "null or time out "
+            LogUtil.e(TAG, "time out null launch ...")
+            val timeOut = timeOutOrNull()
+            LogUtil.e(TAG, "time out null result ---> $timeOut")
+        }
+        jobTimeOutNull.cancel()
+
+        // time out
         GlobalScope.launch {
-            job()
+            TAG = "time out "
+            LogUtil.e(TAG, "time out launch --->")
+
+            val timeout = timeout()
+            LogUtil.e(TAG, "time out ---> $timeout")
+        }
+
+        // test synchronization
+        val launchSynchronization = GlobalScope.launch {
+            TAG = "test synchronization"
+            LogUtil.e(TAG, "synchronization launch ...")
+            val testSynchronization1 = testSynchronization1()
+            val testSynchronization2 = testSynchronization2()
+            LogUtil.e(TAG, "synchronization launch --- result ---> $testSynchronization1 --- $testSynchronization2")
+        }
+        launchSynchronization.cancel()
+
+        // test asynchronous
+        GlobalScope.launch {
+            TAG = "test-asynchronous"
+            LogUtil.e(TAG, "asynchronous launch ...")
+            val startTime = System.currentTimeMillis()
+            val async1 = async {
+                testAsynchronous1()
+            }
+            val async2 = async {
+                testAsynchronous2()
+            }
+            val intervalTime = System.currentTimeMillis() - startTime
+            LogUtil.e(TAG, "asynchronous launch --- result ---> ${async1.await()} --- ${async2.await()}" + "   intervalTime:  " + intervalTime)
         }
     }
 
-    private fun main() = runBlocking<Unit> { // 1：开始执行一个阻塞的协程
-        LogUtil.e(TAG, "开始执行阻塞协程")
-        GlobalScope.launch { // 2: 启动一个新的协程
-            LogUtil.e(TAG, "启动了一个新的协程")
+    private suspend fun doIO() {
+        withContext(Dispatchers.IO) {
+            Thread.sleep(1000)
+            Log.e("Tag", "xxxxxx2 in " + Thread.currentThread().name)
         }
-        LogUtil.e(TAG, "主线程执行")
     }
 
-    private suspend fun job() {
-        val job = GlobalScope.launch {
-            LogUtil.e(TAG, "hello ")
+    private suspend fun timeOutOrNull(): String? {
+        val withTimeoutOrNull = withTimeoutOrNull(4000) {
+            delay(3999)
+            "time out null"
         }
-        job.join()
-        LogUtil.e(TAG, "word ")
+        return withTimeoutOrNull
     }
 
+    private suspend fun timeout(): String {
+        val timeOut = 4001L
+        val withTimeout = withTimeout(4000) {
+            delay(timeOut)
+            "time out "
+        }
+        return withTimeout
+    }
+
+    private suspend fun testSynchronization1(): String {
+        delay(2000)
+        return "test - 1"
+    }
+
+    private suspend fun testSynchronization2(): String {
+        delay(2000)
+        return "test - 2"
+    }
+
+    private suspend fun testAsynchronous1(): String {
+        delay(2000)
+        return "test - 1"
+    }
+
+    private suspend fun testAsynchronous2(): String {
+        delay(2000)
+        return "test - 2"
+    }
 }
