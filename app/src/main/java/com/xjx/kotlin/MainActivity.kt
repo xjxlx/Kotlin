a2190d142bea9f231c1e8fdc27e6aa0f26afd731
xjxliuxing <xjxliuxing@163.com>
2023-03-10 13:14:34 +0800

    xc -3 ->>>  start

--- app/src/main/java/com/xjx/kotlin/MainActivity.kt
+++ app/src/main/java/com/xjx/kotlin/MainActivity.kt
@@ -11,13 +11,14 @@ import com.android.helper.utils.permission.RxPermissionsUtil
 import com.xjx.kotlin.databinding.ActivityMainBinding
 import com.xjx.kotlin.ui.activity.test.*
 import com.xjx.kotlin.ui.activity.test.xc.XC2Activity
+import com.xjx.kotlin.ui.activity.test.xc.XC3Activity
 import com.xjx.kotlin.ui.activity.test.xc.XCActivity
 
 class MainActivity : AppBaseBindingTitleActivity<ActivityMainBinding>() {
 
     override fun initListener() {
         super.initListener()
-        setonClickListener(R.id.tv_item_test_viewpager2, R.id.tv_item_array, R.id.tv_item_list, R.id.tv_item_fun, R.id.tv_item_class, R.id.tv_item_convert_data, R.id.tv_item_xc, R.id.tv_item_aidl, R.id.tv_item_control, R.id.tv_item_fx, R.id.tv_item_fs, R.id.tv_item_xc_2)
+        setonClickListener(R.id.tv_item_test_viewpager2, R.id.tv_item_array, R.id.tv_item_list, R.id.tv_item_fun, R.id.tv_item_class, R.id.tv_item_convert_data, R.id.tv_item_xc, R.id.tv_item_aidl, R.id.tv_item_control, R.id.tv_item_fx, R.id.tv_item_fs, R.id.tv_item_xc_2, R.id.tv_item_xc_3)
     }
 
     override fun initData(savedInstanceState: Bundle?) {
@@ -85,6 +86,9 @@ class MainActivity : AppBaseBindingTitleActivity<ActivityMainBinding>() {
 
             R.id.tv_item_xc_2 -> {
                 startActivity(XC2Activity::class.java)
+            }
+            R.id.tv_item_xc_3 -> {
+                startActivity(XC3Activity::class.java)
             }
         }
     }
