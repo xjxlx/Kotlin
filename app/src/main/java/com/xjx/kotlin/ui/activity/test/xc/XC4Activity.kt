ff5cbd868a9a30dbf489b27c6c80e5aa690a0c62
xjxliuxing <xjxliuxing@163.com>
2023-03-13 19:38:01 +0800

    xc - 4  update --->

--- app/src/main/java/com/xjx/kotlin/ui/activity/test/xc/XC4Activity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/test/xc/XC4Activity.kt
@@ -5,6 +5,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.android.helper.base.title.AppBaseBindingTitleActivity
 import com.xjx.kotlin.databinding.ActivityXc4Binding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.launch
 
 class XC4Activity : AppBaseBindingTitleActivity<ActivityXc4Binding>() {
 
@@ -17,7 +22,70 @@ class XC4Activity : AppBaseBindingTitleActivity<ActivityXc4Binding>() {
     }
 
     override fun initData(savedInstanceState: Bundle?) {
+        val channel = Channel<Int>(1)
 
+        val rendezvousChannel = Channel<String>()
+        val bufferedChannel = Channel<String>(10)
+        val conflatedChannel = Channel<String>(CONFLATED)
+        val unlimitedChannel = Channel<String>(UNLIMITED)
+
+        GlobalScope.launch {
+
+//            launch {
+//                // 这里可能是消耗大量 CPU 运算的异步逻辑，我们将仅仅做 5 次整数的平方并发送
+//                for (x in 1..5) {
+//                    channel.send(x * x)
+//                }
+//            }
+//
+//            // 这里我们打印了 5 次被接收的整数：
+//            launch {
+//                repeat(5) {
+//                  println(channel.receive())
+//                }
+//                println("Done!")
+//            }
+
+            // test  iteration
+            // 这里可能是消耗大量 CPU 运算的异步逻辑，我们将仅仅做 5 次整数的平方并发送
+//            GlobalScope.launch {
+//                for (x in 1..5) {
+//                    channel.send(x * x)
+//                }
+//                channel.close()
+//            }
+
+//            GlobalScope.launch {
+//                val iterator = channel.iterator()
+//                while (iterator.hasNext()) {
+//                    val next = iterator.next()
+//                    LogUtil.e(next)
+//                }
+//                LogUtil.e("Done!")
+
+//                for (item in channel) {
+//                    LogUtil.e(item)
+//                }
+//                LogUtil.e("Done!")
+//            }
+
+            // test close
+//            launch {
+//                // 这里可能是消耗大量 CPU 运算的异步逻辑，我们将仅仅做 5 次整数的平方并发送
+//                for (x in 1..5){
+//                    channel.send(x * x)
+//                }
+//                // 结束发送，并停止阻塞
+//                channel.close()
+//            }
+//
+//            for (y in channel) {
+//                LogUtil.e(y)
+//            }
+//            LogUtil.e("Done!")
+
+            // 测试生产者和消费者
+        }
     }
 
 }
