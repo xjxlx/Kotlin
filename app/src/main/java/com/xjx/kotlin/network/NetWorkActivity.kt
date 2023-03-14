9bf1bb0901d0ee29edab7c4f323f30b3f377a938
xjxliuxing <xjxliuxing@163.com>
2023-03-14 18:08:35 +0800

    network ---> start

---
+++ app/src/main/java/com/xjx/kotlin/network/NetWorkActivity.kt
@@ -0,0 +1,25 @@
+package com.xjx.kotlin.network
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.android.helper.base.title.AppBaseBindingTitleActivity
+import com.xjx.kotlin.databinding.ActivityNetWorkBinding
+
+/**
+ * 网络封装
+ */
+class NetWorkActivity : AppBaseBindingTitleActivity<ActivityNetWorkBinding>() {
+
+    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): ActivityNetWorkBinding {
+        return ActivityNetWorkBinding.inflate(inflater, container, true)
+    }
+
+    override fun setTitleContent(): String {
+        return "网络封装"
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+    }
+
+}
