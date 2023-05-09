57d8470410245692231e6bd72c4657f405329ad3
xjxliuxing <xjxliuxing@163.com>
2023-05-09 19:01:08 +0800

    update  permission

--- app/src/main/java/com/xjx/kotlin/ui/activity/test/flow/FlowCallActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/test/flow/FlowCallActivity.kt
@@ -1,20 +1,15 @@
 package com.xjx.kotlin.ui.activity.test.flow
 
-import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import com.android.apphelper2.utils.LogUtil
-import com.android.apphelper2.utils.permission.PermissionCallBackListener
-import com.android.apphelper2.utils.permission.PermissionRationaleCallBackListener
 import com.android.apphelper2.utils.permission.PermissionUtil
 import com.android.helper.base.title.AppBaseBindingTitleActivity
 import com.xjx.kotlin.databinding.ActivityFlowCallBinding
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.sample
 import kotlinx.coroutines.launch
 
@@ -35,62 +30,23 @@ class FlowCallActivity : AppBaseBindingTitleActivity<ActivityFlowCallBinding>() 
         super.initListener()
         mBinding.btnStart.setOnClickListener {
 
-//            lifecycleScope.launch {
-//                repeat(100) {
-//                    LogUtil.e("sample ---> send ---> ", " send ---> $it")
-//                    mSharedFlow.emit(it)
-//                    delay(1000)
-//                }
-//            }
+            lifecycleScope.launch {
 
-            mPermission.shouldShow(Manifest.permission.READ_EXTERNAL_STORAGE, object : PermissionRationaleCallBackListener {
-                override fun onCallBack(permission: String, rationale: Boolean) {
-                    LogUtil.e("--->permission  : rationale: $rationale")
-
-                    mPermission.setCallBackListener(object : PermissionCallBackListener {
-                        override fun onCallBack(permission: String, isGranted: Boolean) {
-                            LogUtil.e("--->permission  : isGranted: $isGranted")
-
-                        }
-                    })
-                        .request(Manifest.permission.READ_EXTERNAL_STORAGE)
+                repeat(100) {
+                    mSharedFlow.emit(it)
+                    delay(100)
                 }
-            })
+            }
         }
     }
 
     override fun initData(savedInstanceState: Bundle?) {
-//        val flow = callbackFlow {
-//            requestApi {
-//                // 发送成功的数据
-//                trySend("2")
-//
-//                // 发送错误的数据
-//                close(NullPointerException("error"))
-//            }
-//
-//            // 关闭的回调
-//            awaitClose {
-//                // 关闭的回调
-//                LogUtil.e("awaitClose ---->")
-//            }
-//        }
 
         lifecycleScope.launch {
-            mSharedFlow.sample(3000)
-                .debounce(1000)
-                .onEach {
-
-                }
+            mSharedFlow.sample(1000)
                 .collect() {
                     LogUtil.e("sample ---> result ---> ", "result ---> $it")
                 }
         }
-
-        val flow = flow<Int> {
-            emit(1)
-        }
-
     }
-
 }
