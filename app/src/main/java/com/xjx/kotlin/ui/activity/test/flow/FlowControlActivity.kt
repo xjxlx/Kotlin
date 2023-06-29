370ae59760f8bed9b0fe06e40d5159eee5ff2214
xjxliuxing <xjxliuxing@163.com>
2023-06-29 14:58:58 +0800

    add record video code

--- app/src/main/java/com/xjx/kotlin/ui/activity/test/flow/FlowControlActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/test/flow/FlowControlActivity.kt
@@ -3,10 +3,17 @@ package com.xjx.kotlin.ui.activity.test.flow
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.android.helper.base.title.AppBaseBindingTitleActivity
+import com.android.helper.utils.LogUtil
 import com.xjx.kotlin.databinding.ActivityFlowControlBinding
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.launch
 
 class FlowControlActivity : AppBaseBindingTitleActivity<ActivityFlowControlBinding>() {
+
+    private val mFlow: MutableSharedFlow<Boolean> = MutableSharedFlow()
 
     override fun setTitleContent(): String {
         return "Flow 操作符"
@@ -17,5 +24,21 @@ class FlowControlActivity : AppBaseBindingTitleActivity<ActivityFlowControlBindi
     }
 
     override fun initData(savedInstanceState: Bundle?) {
+        mBinding.btnStart.setOnClickListener {
+            lifecycleScope.launch {
+                LogUtil.e("debounce", "发送数据！")
+                mFlow.emit(true)
+            }
+        }
+
+        // 1: 超时操作符
+        lifecycleScope.launch {
+            mFlow.debounce(1000 * 5)
+                .collect {
+                    LogUtil.e("debounce", "初始化数据！")
+                }
+        }
     }
+
+
 }
