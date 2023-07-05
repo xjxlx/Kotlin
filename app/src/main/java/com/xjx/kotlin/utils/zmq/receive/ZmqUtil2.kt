50352ae64d8870a0d41adb894dbb8ebd58c54623
xjxliuxing <xjxliuxing@163.com>
2023-07-05 14:44:31 +0800

    update code

--- app/src/main/java/com/xjx/kotlin/utils/zmq/receive/ZmqUtil2.kt
+++ app/src/main/java/com/xjx/kotlin/utils/zmq/receive/ZmqUtil2.kt
@@ -63,6 +63,10 @@ object ZmqUtil2 {
         }
 
         runCatching {
+            val iPv6 = getSocket().isIPv6
+            val iPv4Only = getSocket().iPv4Only
+            log("iPv6 is: $iPv6 iPv4Only: $iPv4Only")
+
             mBinding = getSocket().bind(TCP.TCP_ADDRESS)
             log("bind address:[ $TCP.ADDRESS ]   ---> bind success：$mBinding")
 
