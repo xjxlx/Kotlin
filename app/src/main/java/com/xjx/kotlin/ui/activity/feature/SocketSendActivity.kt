71dcf61843daf3140cf30fa382d0f92256a2eda7
xjxliuxing <xjxliuxing@163.com>
2023-07-09 01:01:28 +0800

    update code

--- app/src/main/java/com/xjx/kotlin/ui/activity/feature/SocketSendActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/feature/SocketSendActivity.kt
@@ -1,17 +1,14 @@
 package com.xjx.kotlin.ui.activity.feature
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.android.helper.base.title.AppBaseBindingTitleActivity
-import com.xjx.kotlin.R
 import com.xjx.kotlin.databinding.ActivitySocketSendBinding
 
 class SocketSendActivity : AppBaseBindingTitleActivity<ActivitySocketSendBinding>() {
 
     override fun initData(savedInstanceState: Bundle?) {
-
     }
 
     override fun setTitleContent(): String {
