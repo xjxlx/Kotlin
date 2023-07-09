9b352df45d249d1e619420b83d7b50c45a2399fe
xjxliuxing <xjxliuxing@163.com>
2023-07-09 14:14:27 +0800

    update code

--- app/src/main/java/com/xjx/kotlin/ui/activity/feature/SocketResultActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/feature/SocketResultActivity.kt
@@ -3,10 +3,15 @@ package com.xjx.kotlin.ui.activity.feature
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.android.apphelper2.utils.SocketUtil
 import com.android.helper.base.title.AppBaseBindingTitleActivity
 import com.xjx.kotlin.databinding.ActivitySocketResultBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SocketResultActivity : AppBaseBindingTitleActivity<ActivitySocketResultBinding>() {
+    private val socketUtil = SocketUtil.SocketService()
 
     override fun setTitleContent(): String {
         return "Socket - 接收端"
@@ -18,5 +23,29 @@ class SocketResultActivity : AppBaseBindingTitleActivity<ActivitySocketResultBin
 
     override fun initData(savedInstanceState: Bundle?) {
 
+        socketUtil.setServiceCallBackListener(object : SocketUtil.SocketService.ServerCallBackListener {
+            override fun callBack(send: String, result: String) {
+                mBinding.tvSend.post {
+                    mBinding.tvSend.text = send
+                }
+                mBinding.tvResult.post {
+                    mBinding.tvResult.text = result
+                }
+            }
+        })
+
+        mBinding.btnInitSocket.setOnClickListener {
+            socketUtil.initSocketService()
+        }
+
+
+        mBinding.btnSend.setOnClickListener {
+            lifecycleScope.launch {
+                repeat(Int.MAX_VALUE) {
+                    socketUtil.sendServerData("服务端-->发送：$it")
+                    delay(200)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/xjx/kotlin/ui/activity/feature/SocketSendActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/feature/SocketSendActivity.kt
@@ -1,15 +1,20 @@
 package com.xjx.kotlin.ui.activity.feature
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.android.apphelper2.utils.SocketUtil
+import com.android.apphelper2.utils.ToastUtil
 import com.android.helper.base.title.AppBaseBindingTitleActivity
 import com.xjx.kotlin.databinding.ActivitySocketSendBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SocketSendActivity : AppBaseBindingTitleActivity<ActivitySocketSendBinding>() {
 
-    override fun initData(savedInstanceState: Bundle?) {
-    }
+    private val socketUtil = SocketUtil.SocketClient()
 
     override fun setTitleContent(): String {
         return "Socket - 发送端"
@@ -17,5 +22,37 @@ class SocketSendActivity : AppBaseBindingTitleActivity<ActivitySocketSendBinding
 
     override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): ActivitySocketSendBinding {
         return ActivitySocketSendBinding.inflate(inflater, container, true)
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+        socketUtil.setServiceCallBackListener(object : SocketUtil.SocketClient.ClientCallBackListener {
+            override fun callBack(send: String, result: String) {
+                mBinding.tvSend.post {
+                    mBinding.tvSend.text = send
+                }
+                mBinding.tvResult.post {
+                    mBinding.tvResult.text = result
+                }
+            }
+        })
+
+
+        mBinding.btnInitSocket.setOnClickListener {
+            val ip = mBinding.etIp.text.toString()
+            if (TextUtils.isEmpty(ip)) {
+                ToastUtil.show("Ip不能为空！")
+                return@setOnClickListener
+            }
+            socketUtil.initClientSocket(ip)
+        }
+
+        mBinding.btnSend.setOnClickListener {
+            lifecycleScope.launch {
+                repeat(Int.MAX_VALUE) {
+                    socketUtil.sendClientData("客户端-->发送：$it")
+                    delay(200)
+                }
+            }
+        }
     }
 }
