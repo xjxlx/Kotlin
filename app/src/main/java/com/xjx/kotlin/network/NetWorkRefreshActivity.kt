46a6bbdb3599e3877897c51edc0b1d3b9a846b63
xjxliuxing <xjxliuxing@163.com>
2023-08-03 01:08:22 +0800

    update code

--- app/src/main/java/com/xjx/kotlin/network/NetWorkRefreshActivity.kt
+++ app/src/main/java/com/xjx/kotlin/network/NetWorkRefreshActivity.kt
@@ -8,16 +8,16 @@ import com.android.apphelper2.base.BaseBindingTitleActivity
 import com.android.apphelper2.base.recycleview.BaseRecycleViewFragment
 import com.android.apphelper2.base.recycleview.BaseVH
 import com.android.apphelper2.utils.RecycleUtil
-import com.android.common.utils.LogUtil
 import com.android.http.utils.client.HttpClient
 import com.android.http.utils.client.RetrofitHelper
-import com.android.http.utils.listener.HttpCallBackListener
 import com.android.http.utils.test.HttpResponse
 import com.android.http.utils.test.L6HomeRightBookListBean
 import com.android.http.utils.test.TestApiService
+import com.android.refresh.utils.CallBackListener
+import com.android.refresh.utils.RefreshUtil
 import com.xjx.kotlin.databinding.ActivityNetWorkRefreshBinding
 import com.xjx.kotlin.databinding.ItemNetRefreshBinding
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
 
 class NetWorkRefreshActivity : BaseBindingTitleActivity<ActivityNetWorkRefreshBinding>() {
 
@@ -31,77 +31,74 @@ class NetWorkRefreshActivity : BaseBindingTitleActivity<ActivityNetWorkRefreshBi
             .setVertical()
             .setAdapter(adapter)
 
-//        object : RefreshUtil<String>() {
-//            override fun setNoMoreData(t: String): List<*> {
-//                return getCurrentData()
-//            }
-//        }.with(this, mBinding.brlLayout)
-//            .setCallBackListener(object : RefreshCallBack<String>() {
-//                override fun onSuccess(refreshUtil: RefreshUtil<String>, t: String) {
-//                }
-//
-//                override fun onError(e: Throwable) {
-//                }
-//            })
-//            .execute()
-
-//        object : RefreshUtil<String>() {
-//            override fun setNoMoreData(t: String): List<*> {
-//               return
-//            }
-//        }.apply {
-//            setCallBackListener(object : RefreshCallBack<String>() {
-//                override fun onSuccess(refreshUtil: RefreshUtil<String>, t: String) {
-//                }
-//
-//                override fun onError(e: Throwable) {
-//                }
-//            })
-//            setAutoRefresh(true)
-//            execute()
-//        }
-
         mBinding.btnStart.setOnClickListener {
             mList.clear()
-            lifecycleScope.launch {
-                val unId = "o9RWl1EJPHolk8_7smU39k1-LqVs"
-                val suiJi = "newcL6_2"
-                val mParameters = mutableMapOf<String, Any>()
-                mParameters["service"] = "App.App2022.GetBook"
-                mParameters["unid"] = unId
-                mParameters["suiji"] = suiJi
 
-                HttpClient.http<TestApiService, MutableMap<String, Any>, HttpResponse<L6HomeRightBookListBean>>({ getL6BookList(it) },
-                    mParameters, object : HttpCallBackListener<HttpResponse<L6HomeRightBookListBean>>() {
+            val unId = "o9RWl1EJPHolk8_7smU39k1-LqVs"
+            val suiJi = "newcL6_2"
+            val mParameters = mutableMapOf<String, Any>()
+            mParameters["service"] = "App.App2022.GetBook"
+            mParameters["unid"] = unId
+            mParameters["suiji"] = suiJi
 
-                        override fun onFailure(exception: Throwable) {
-                            super.onFailure(exception)
-                            LogUtil.e("sss", "error:${exception}")
-                        }
+            object : RefreshUtil<HttpResponse<L6HomeRightBookListBean>>(lifecycleScope, mBinding.brlLayout) {
+                override suspend fun getApiService(): Flow<HttpResponse<L6HomeRightBookListBean>> {
+                    return HttpClient.http<TestApiService, MutableMap<String, Any>, HttpResponse<L6HomeRightBookListBean>>(
+                        { getL6BookList(it) }, mParameters)
+                }
 
-                        override fun onSuccess(t: HttpResponse<L6HomeRightBookListBean>) {
-                            LogUtil.e("sss", t)
-                            t.data?.row2?.let {
-                                for (index in it.indices) {
-                                    val row2 = it[index]
-                                    row2?.let { row ->
-                                        row.content?.let { content ->
-                                            for (index2 in content.indices) {
-                                                val content1 = content[index2]
-                                                content1?.let { content11 ->
-                                                    content11.content_name?.let { name ->
-                                                        mList.add(name)
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            adapter.setList(mList)
-                        }
-                    })
-            }
+                override fun setMoreData(t: HttpResponse<L6HomeRightBookListBean>): List<*>? {
+                    return t.data?.row1
+                }
+            }.setCallBackListener(object : CallBackListener<HttpResponse<L6HomeRightBookListBean>>() {
+                override fun onSuccess(refreshUtil: RefreshUtil<HttpResponse<L6HomeRightBookListBean>>,
+                                       t: HttpResponse<L6HomeRightBookListBean>) {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+                .start()
+
+//            lifecycleScope.launch {
+//                val unId = "o9RWl1EJPHolk8_7smU39k1-LqVs"
+//                val suiJi = "newcL6_2"
+//                val mParameters = mutableMapOf<String, Any>()
+//                mParameters["service"] = "App.App2022.GetBook"
+//                mParameters["unid"] = unId
+//                mParameters["suiji"] = suiJi
+//
+//                HttpClient.http<TestApiService, MutableMap<String, Any>, HttpResponse<L6HomeRightBookListBean>>({ getL6BookList(it) },
+//                    mParameters, object : HttpCallBackListener<HttpResponse<L6HomeRightBookListBean>>() {
+//
+//                        override fun onFailure(exception: Throwable) {
+//                            super.onFailure(exception)
+//                            LogUtil.e("sss", "error:${exception}")
+//                        }
+//
+//                        override fun onSuccess(t: HttpResponse<L6HomeRightBookListBean>) {
+//                            LogUtil.e("sss", t)
+//                            t.data?.row2?.let {
+//                                for (index in it.indices) {
+//                                    val row2 = it[index]
+//                                    row2?.let { row ->
+//                                        row.content?.let { content ->
+//                                            for (index2 in content.indices) {
+//                                                val content1 = content[index2]
+//                                                content1?.let { content11 ->
+//                                                    content11.content_name?.let { name ->
+//                                                        mList.add(name)
+//                                                    }
+//                                                }
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                            adapter.setList(mList)
+//                        }
+//                    })
+//            }
         }
     }
 
