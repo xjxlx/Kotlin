555a7a19a8ffcc1bd0ffb6d742406733bb9b0ea5
xjxliuxing <xjxliuxing@163.com>
2023-08-17 23:34:37 +0800

    update code

--- app/src/main/java/com/xjx/kotlin/ui/activity/custom/CustomTabLayoutActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/custom/CustomTabLayoutActivity.kt
@@ -6,16 +6,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.android.apphelper2.base.BaseBindingTitleActivity
+import com.android.apphelper2.widget.TabLayout
 import com.android.common.base.BaseViewPager2FragmentAdapter
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 import com.xjx.kotlin.databinding.ActivityCustomTabLayoutBinding
 import com.xjx.kotlin.ui.activity.fragments.ClueClueFragment
 import com.xjx.kotlin.ui.activity.fragments.ClueNeedFragment
 
 class CustomTabLayoutActivity : BaseBindingTitleActivity<ActivityCustomTabLayoutBinding>() {
 
-    //    private val mTitleArray: Array<String> = arrayOf("item1", "item2", "item33333", "item")
+    // private val mTitleArray: Array<String> = arrayOf("item1", "item2", "item33333", "item")
     private val mTitleArray: Array<String> = arrayOf("线索", "需求", "商场111111", "我的", "我的")
 
     private val mFragmentList: MutableList<Fragment> = mutableListOf<Fragment>().apply {
@@ -23,35 +22,21 @@ class CustomTabLayoutActivity : BaseBindingTitleActivity<ActivityCustomTabLayout
         add(ClueNeedFragment.newInstance())
         add(ClueNeedFragment.newInstance())
         add(ClueNeedFragment.newInstance())
+        add(ClueClueFragment.newInstance())
     }
 
     override fun initData(savedInstanceState: Bundle?) {
         val adapter = BaseViewPager2FragmentAdapter(this.supportFragmentManager, this.lifecycle, mFragmentList)
         mBinding.vp.adapter = adapter
 
-//        TabLayoutUtil.Builder()
-//            .setViewPager2TitleList(mTitleArray2)
-//            .Build()
-//            .setupWithViewPager(mBinding.vp, mBinding.tbLayout)
-//
-        // 1: add tab
-        for (element in mTitleArray) {
-            mBinding.tbLayout.addTab(mBinding.tbLayout.newTab()
-                .setText(element))
-        }
-        // 2：滑动方式
-        mBinding.tbLayout.tabMode = TabLayout.MODE_FIXED // tablelayout的滑动方式，这里只有两个，所以设置为固定模式
-
-        // 4:关联TabLayout 和 ViewPager
-        TabLayoutMediator(mBinding.tbLayout, mBinding.vp, object : TabLayoutMediator.TabConfigurationStrategy {
-            override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
-                tab.text = mTitleArray[position]
-            }
-        }).attach()
-
-        val mItemTitleArray: Array<String> = arrayOf("线索", "需求", "商场111111", "我的")
         mBinding.tb.setItemBackgroundColor(Color.YELLOW)
-            .withViewPager2(mBinding.vp, mBinding.tbLayout)
+            .setItemSize(50F)
+            .withViewPager2(mBinding.vp, mTitleArray)
+            .setSelectorListener(object : TabLayout.SelectorListener {
+                override fun onSelector(position: Int) {
+                    mBinding.vp.currentItem = position
+                }
+            })
     }
 
     override fun getBinding(inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean): ActivityCustomTabLayoutBinding {
