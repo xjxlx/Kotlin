d834e1409b44d6bd1b91709c18702edad4f83ee1
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-06 21:57:46 +0800

    add flavorDimensions logic

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,9 +1,30 @@
+//plugins {
+//    `kotlin-dsl`
+//}
+//
+//repositories {
+//    // The org.jetbrains.kotlin.jvm plugin requires a repository
+//    // where to download the Kotlin compiler dependencies from.
+//    mavenCentral()
+//}
+//
+//dependencies {
+//    implementation("com.android.tools.build:gradle-api:8.0.2")
+//    implementation(kotlin("stdlib", "1.8.10"))
+//    gradleApi()
+//}
+
+
 plugins {
     `kotlin-dsl`
+//    kotlin("jvm") version "1.8.0"
 }
-
 repositories {
-    // The org.jetbrains.kotlin.jvm plugin requires a repository
-    // where to download the Kotlin compiler dependencies from.
+    google()
     mavenCentral()
+}
+dependencies {
+//    implementation("com.android.tools.build:gradle-api:8.0.2")
+//    implementation(kotlin("stdlib", "1.8.10"))
+    gradleApi()
 }
