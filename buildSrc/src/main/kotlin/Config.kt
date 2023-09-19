dd287ae54f690c29b78fdfa4128fbfb8f7945137
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-19 23:18:07 +0800

    add flavorDimensions logic

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,10 +1,10 @@
 object Config {
-    const val compileSdk = 31
+    const val compileSdk = 30
     const val compileSdkVersion = "30"
 
-    const val buildToolsVersion = "29.0.2"
+    const val buildToolsVersion = "30.0.3"
     const val minSdk = 30
-    const val targetSdkVersion = 31
+    const val targetSdkVersion = 30
     const val versionCode = 2
     const val versionName = "1.0.0"
 }
