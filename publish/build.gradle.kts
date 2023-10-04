295dca33818fdc6f9d631f323612cf2d6c4948a1
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-04 21:44:08 +0800

    add flavorDimensions logic

--- publish/build.gradle.kts
+++ publish/build.gradle.kts
@@ -2,23 +2,23 @@
 plugins {
 //    id("com.android.library")
     // 使用 Gradle 发行版附带的 Java Gradle 插件开发插件来创作您的 Gradle 插件。
-//    id("java-gradle-plugin")
+    id("java-gradle-plugin")
     // 使用 Maven 发布插件为您的插件生成已发布的元数据
     id("maven-publish")
     id("org.jetbrains.kotlin.jvm") // 用kotlin语言来开发
     // 最新版本的插件发布插件可以在 Gradle 插件门户上找到。
-    id("com.gradle.plugin-publish") version "1.0.0-rc-1"
+//    id("com.gradle.plugin-publish") version "1.0.0-rc-1"
 }
 
 // 2：配置发布到远程仓库的地址信息
-pluginBundle {
-    // 为您的插件项目设置网站。
-    website = "https://github.com/xjxlx/plugins/blob/main/publish/README.md"
-    // 提供源存储库 URI，以便其他人在想要贡献时可以找到它。
-    vcsUrl = "https://github.com/xjxlx/plugins"
-    // 设置要用于所有插件的标签，除非在块中被覆盖。plugins,插件的tag。可以通过这个在插件门户上搜索
-    tags = listOf("publish", "android", "plugins")
-}
+//pluginBundle {
+//    // 为您的插件项目设置网站。
+//    website = "https://github.com/xjxlx/plugins/blob/main/publish/README.md"
+//    // 提供源存储库 URI，以便其他人在想要贡献时可以找到它。
+//    vcsUrl = "https://github.com/xjxlx/plugins"
+//    // 设置要用于所有插件的标签，除非在块中被覆盖。plugins,插件的tag。可以通过这个在插件门户上搜索
+//    tags = listOf("publish", "android", "plugins")
+//}
 
 // 3：配置版本的group 和 version
 /**
@@ -29,23 +29,23 @@ pluginBundle {
  *      4:如果是要发布到gradle的门户网站上，需要使用io.github.{userName}作为域名
  */
 group = "io.github.xjxlx"
-version = "1.0.2"
+version = "1.0.9"
 
 // 4：配置发布插件的信息
-gradlePlugin {
-    plugins {
-        create("greetingsPlugin") {
-            // id：必须是以group作为开头的
-            id = "${group}.publish"
-            // 简称
-            displayName = "publishPlugin"
-            // 简介
-            description = "<Good human-readable description of what your plugin is about>"
-            // 插件路径，必须是以全路径作为名称
-            implementationClass = "com.android.plugin.plugin.PublishPlugin"
-        }
-    }
-}
+//gradlePlugin {
+//    plugins {
+//        create("greetingsPlugin") {
+//            // id：必须是以group作为开头的
+//            id = "${group}.publish"
+//            // 简称
+//            displayName = "publishPlugin"
+//            // 简介
+//            description = "<Good human-readable description of what your plugin is about>"
+//            // 插件路径，必须是以全路径作为名称
+//            implementationClass = "com.android.plugin.plugin.PublishPlugin"
+//        }
+//    }
+//}
 
 /**
  * 5：如果需要配置到本地环境的话，需要加入本地的maven
--- publish/src/main/java/com/android/plugin/plugin/PublishPlugin.kt
+++ publish/src/main/java/com/android/plugin/plugin/PublishPlugin.kt
@@ -13,17 +13,30 @@ import org.gradle.api.publish.maven.MavenPublication
 class PublishPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
+        println("---------------------》")
+
         // 1：注册一个片段，用来传输数据使用
         val publishExtension = project.extensions.create("publishExtension", PublishPluginExtension::class.java)
+//            .apply {
+//                publishGroupId.convention("com.android.helper")
+//                publishArtifactId.convention("publish")
+//                publishVersion.convention("1.0.0")
+//            }
+
+        val publishGroupId = publishExtension.publishGroupId.get()
+        val publishArtifactId = publishExtension.publishArtifactId.get()
+        val publishVersion = publishExtension.publishVersion.get()
 
         // 注册一个发布的类型
         registerPublishType(project)
 
         // 2：检查是否安装了push插件
         val plugin = project.pluginManager.findPlugin("maven-publish")
+        println("maven-publish 是否为空：$plugin")
         if (plugin == null) {
             // 安装插件
             project.pluginManager.apply("maven-publish")
+            println("添加 maven-publish ！")
 
             // String id = plugin.getId();
             // String name = plugin.getName();
@@ -33,13 +46,15 @@ class PublishPlugin : Plugin<Project> {
             // println("name:" + name);
             // PluginContainer plugins = project.getPlugins();
         }
+        println("maven-publish 是否为空：${project.pluginManager.findPlugin("maven-publish")}")
+
+        println("before---> groupId:$publishGroupId artifactId:$publishArtifactId version:$publishVersion")
 
         // 3：注册一个发布的task
         project.task("publishTask") {
             it.group = "install"
             // 发布插件
-            publishTask(project, publishExtension.publishGroupId.get(), publishExtension.publishArtifactId.get(),
-                publishExtension.publishVersion.get())
+            publishTask(project, publishGroupId, publishArtifactId, publishVersion)
         }
     }
 
@@ -65,19 +80,26 @@ class PublishPlugin : Plugin<Project> {
      */
     private fun publishTask(project: Project, groupId: String, artifactId: String, version: String) {
         // 获取插件版本信息
-        println("groupId:$groupId artifactId:$artifactId version:$version")
+        println("install---> groupId:$groupId artifactId:$artifactId version:$version")
 
         // 在所有的配置都完成之后执行
         project.afterEvaluate { project1: Project ->
             val publish = project1.extensions.getByType(PublishingExtension::class.java)
             publish.publications.create("release", MavenPublication::class.java, Action { maven: MavenPublication ->
-                maven.groupId = groupId
-                maven.artifactId = artifactId
+
+//                groupId = "com.android.helper"
+////                artifactId = getModelNameForNamespace() // 插件名称
+////                version = latestGitTag().ifEmpty { Config.versionName } // 版本号
+
+                maven.groupId = "com.github.xjxlx"
+                maven.artifactId = "dimens"
                 var gitVersion = VersionUtil.VERSION
+                println("gitVersion:$gitVersion")
                 if (TextUtil.isEmpty(gitVersion)) {
                     gitVersion = version
                 }
                 maven.version = gitVersion
+                println("release---> groupId:${maven.groupId} artifactId:${maven.artifactId} version:${maven.version}")
 
                 // 发布
                 maven.from(project1.components.getByName("release"))
