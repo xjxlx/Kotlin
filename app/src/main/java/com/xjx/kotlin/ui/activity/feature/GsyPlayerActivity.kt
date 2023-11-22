1b11a7b7082338a76730796fda4a448a70f69e35
xjxliuxing@163.com <xjxliuxing@163.com>
2023-11-22 23:54:30 +0800

    update files

--- app/src/main/java/com/xjx/kotlin/ui/activity/feature/GsyPlayerActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/feature/GsyPlayerActivity.kt
@@ -8,7 +8,6 @@ import com.android.common.base.BaseBindingTitleActivity
 import com.android.helper.player.app.BaseBuddyVideoPlayer
 import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder
 import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack
-import com.shuyu.gsyvideoplayer.utils.Debuger
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils
 import com.xjx.kotlin.R
 import com.xjx.kotlin.databinding.ActivityGsyPlayerBinding
@@ -23,16 +22,22 @@ class GsyPlayerActivity : BaseBindingTitleActivity<ActivityGsyPlayerBinding>() {
     }
 
     override fun initData(savedInstanceState: Bundle?) {
+        val inflate = LayoutInflater.from(this)
+            .inflate(R.layout.item_gsyplayer, null, false)
+        val video = inflate.findViewById<BaseBuddyVideoPlayer>(R.id.iv_today_video)
 
-        val inflate = layoutInflater.inflate(R.layout.item_test_player, null, false)
-        val videoPlayer = inflate.findViewById<BaseBuddyVideoPlayer>(R.id.bdvp)
 
+        findViewById<ViewGroup>(R.id.ll_parent).addView(inflate)
+
+        player(video)
+    }
+
+    private fun player(video: BaseBuddyVideoPlayer) {
         //外部辅助的旋转，帮助全屏
-        val orientationUtils = OrientationUtils(this, videoPlayer)
+        val orientationUtils = OrientationUtils(this, video)
         //初始化不打开外部的旋转
-        orientationUtils.setEnable(false)
-        val videoUrl = "http://7xjmzj.com1.z0.glb.clouddn.com/20171026175005_JObCxCE2.mp4";
-
+        orientationUtils.isEnable = false
+        val videoUrl = "http://7xjmzj.com1.z0.glb.clouddn.com/20171026175005_JObCxCE2.mp4"
         val gsyVideoOption = GSYVideoOptionBuilder()
         gsyVideoOption.setIsTouchWiget(true)
             .setRotateViewAuto(false)
@@ -47,34 +52,23 @@ class GsyPlayerActivity : BaseBindingTitleActivity<ActivityGsyPlayerBinding>() {
                 override fun onPrepared(url: String, vararg objects: Any) {
                     super.onPrepared(url, *objects)
                     //开始播放了才能旋转和全屏
-                    orientationUtils.setEnable(true)
+                    orientationUtils.isEnable = true
                 }
 
                 override fun onQuitFullscreen(url: String, vararg objects: Any) {
                     super.onQuitFullscreen(url, *objects)
-                    Debuger.printfError("***** onQuitFullscreen **** " + objects[0]) //title
-                    Debuger.printfError("***** onQuitFullscreen **** " + objects[1]) //当前非全屏player
-                    if (orientationUtils != null) {
-                        orientationUtils.backToProtVideo()
-                    }
+                    orientationUtils.backToProtVideo()
                 }
             })
-            .setLockClickListener { view, lock ->
-                if (orientationUtils != null) {
-                    //配合下方的onConfigurationChanged
-                    orientationUtils.setEnable(!lock)
-                }
+            .setLockClickListener { view, lock -> //配合下方的onConfigurationChanged
+                orientationUtils.isEnable = !lock
             }
-            .build(videoPlayer)
-
-        videoPlayer.getFullscreenButton()
-            .setOnClickListener(View.OnClickListener { //直接横屏
-                orientationUtils.resolveByClick()
-
+            .build(video)
+        video.getFullscreenButton()
+            .setOnClickListener(View.OnClickListener {
+                orientationUtils.resolveByClick() //直接横屏
                 //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
-                videoPlayer.startWindowFullscreen(this@GsyPlayerActivity, true, true)
+                video.startWindowFullscreen(mActivity, true, true)
             })
-
-        mBinding.flContent.addView(inflate)
     }
 }
