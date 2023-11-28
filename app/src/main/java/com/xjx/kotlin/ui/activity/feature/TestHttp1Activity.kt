40b90dcdb1bb317bdd095796991710146e5c650a
xjxliuxing@163.com <xjxliuxing@163.com>
2023-11-29 00:13:31 +0800

    update files

--- app/src/main/java/com/xjx/kotlin/ui/activity/feature/TestHttp1Activity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/feature/TestHttp1Activity.kt
@@ -2,85 +2,78 @@ package com.xjx.kotlin.ui.activity.feature
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.lifecycleScope
 import com.android.common.base.BaseBindingTitleActivity
-import com.android.common.base.recycleview.BaseVH
-import com.xjx.kotlin.R
+import com.android.common.utils.LogUtil
+import com.android.http.client.HttpClient
+import com.android.http.listener.HttpCallBackListener
+import com.android.http.test.HttpResponse
+import com.android.http.test.L6HomeRightBookListBean
+import com.android.http.test.TestApiService
 import com.xjx.kotlin.databinding.ActivityTestHttp1Binding
+import kotlinx.coroutines.launch
 
 class TestHttp1Activity : BaseBindingTitleActivity<ActivityTestHttp1Binding>() {
-
-    override fun initData(savedInstanceState: Bundle?) {
-        mBinding.btn1.text = "侧石"
-    }
-
-    internal inner class BaseAdapter<T : BaseVH> {
-        fun convert(t: T) {
-            val baseVH = t as VHChild
-        }
-    }
-
-    internal class VHChild(root: View) : BaseVH(root)
-
-    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean): ActivityTestHttp1Binding {
-        return ActivityTestHttp1Binding.inflate(inflater, container, true)
-    }
 
     override fun getTitleContent(): String {
         return "测试网络连接-1"
     }
 
-    open class VH(view: View) : RecyclerView.ViewHolder(view) {}
+    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean): ActivityTestHttp1Binding {
+        return ActivityTestHttp1Binding.inflate(inflater, container, true)
+    }
 
-    class ChildVH(view: View) : VH(view) {}
+    override fun initData(savedInstanceState: Bundle?) {
+        mBinding.btn1.setOnClickListener {
+            lifecycleScope.launch {
+                val unId = "o9RWl1EJPHolk8_7smU39k1-LqVs"
+                val suiJi = "newcL6_2"
+                val mParameters = mutableMapOf<String, Any>()
+                mParameters["service"] = "App.App2022.GetBook"
+                mParameters["unid"] = unId
+                mParameters["suiji"] = suiJi
 
-    class Empty(view: View) : VH(view) {}
+                HttpClient.http<TestApiService, MutableMap<String, Any>, HttpResponse<L6HomeRightBookListBean>>({ getL6BookList(it) }, mParameters, object : HttpCallBackListener<HttpResponse<L6HomeRightBookListBean>>() {
+                    override fun onFailure(exception: Throwable) {
+                        super.onFailure(exception)
+                        LogUtil.e("sss", "error:${exception}")
+                    }
 
-    class A : BashAdapter2<String, ChildVH>() {
-        override fun bindViewHolder(holder: ChildVH) {
-        }
+                    override fun onSuccess(t: HttpResponse<L6HomeRightBookListBean>) {
+                        LogUtil.e("sss", t)
+                    }
+                })
 
-        override fun createVH(layoutInflater: LayoutInflater, viewType: Int): ChildVH {
-            return ChildVH(layoutInflater.inflate(R.layout.activity_aidl, null, false))
+                HttpClient.http<TestApiService, MutableMap<String, Any>, HttpResponse<L6HomeRightBookListBean>>({ getL6BookList(it) }, mParameters, object : HttpCallBackListener<HttpResponse<L6HomeRightBookListBean>>() {
+                    override fun onSuccess(t: HttpResponse<L6HomeRightBookListBean>) {
+                    }
+                })
+            }
         }
     }
 
-    abstract class BashAdapter2<T, E : RecyclerView.ViewHolder> : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-        //        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): E {
-//            if (viewType == 1) {
-//                return createVH(layoutInflater = LayoutInflater.from(parent.context), viewType)
-//            } else {
-//                val inflate = LayoutInflater.from(parent.context).inflate(R.layout.activity_aidl, null, false)
-//                val empty = Empty(inflate)
-//                return empty
-//            }
-//        }
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            if (viewType == 1) {
-                return createVH(layoutInflater = LayoutInflater.from(parent.context), viewType)
-            } else {
-                val inflate = LayoutInflater.from(parent.context).inflate(R.layout.activity_aidl, null, false)
-                val empty = Empty(inflate)
-                return empty
-            }
+    fun sss() {
+        val launch = lifecycleScope.launch {
+            val api = HttpClient.getApi<TestApiService>()
+            val api1 = HttpClient.getApi<TestApiService>()
         }
 
-        override fun getItemCount(): Int {
-            return 1
+        val api = HttpClient.getApi<TestApiService>()
+
+        lifecycleScope.launch {
+            val mParameters = mutableMapOf<String, Any>()
+            api.getL6BookList(mParameters)
+
+//            HttpClient.http2<TestApiService, MutableMap<String, Any>, HttpResponse<L6HomeRightBookListBean>>({ getL6BookList(mParameters) }, object : HttpCallBackListener<HttpResponse<L6HomeRightBookListBean>>() {
+//                override fun onSuccess(t: HttpResponse<L6HomeRightBookListBean>) {
+//                }
+//            })
+//            HttpClient.http2<TestApiService, MutableMap<String, Any>, HttpResponse<L6HomeRightBookListBean>>({ getL6BookList(mParameters) }, object : HttpCallBackListener<HttpResponse<L6HomeRightBookListBean>>() {
+//                override fun onSuccess(t: HttpResponse<L6HomeRightBookListBean>) {
+//                }
+//            })
+
         }
-
-//        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-//            val holder1 = holder
-//            if (holder is) bindViewHolder(holder1)
-//        }
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        }
-
-        abstract fun createVH(layoutInflater: LayoutInflater, viewType: Int): E
-
-        abstract fun bindViewHolder(holder: E)
     }
 }
