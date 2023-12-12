a929a25bde517b6e22617da51736dd2d76791618
xjxliuxing@163.com <xjxliuxing@163.com>
2023-12-12 14:42:16 +0800

    update js animation

--- app/src/main/java/com/xjx/kotlin/ui/activity/custom/CustomTabLayoutActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/custom/CustomTabLayoutActivity.kt
@@ -7,30 +7,52 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.android.common.base.BaseBindingTitleActivity
 import com.android.common.base.BaseViewPager2FragmentAdapter
+import com.android.common.utils.TabLayoutUtil2
+import com.google.android.material.tabs.TabLayout
 import com.xjx.kotlin.databinding.ActivityCustomTabLayoutBinding
 import com.xjx.kotlin.ui.activity.fragments.ClueNeedFragment
 
 class CustomTabLayoutActivity : BaseBindingTitleActivity<ActivityCustomTabLayoutBinding>() {
 
-    private val mFragmentList: MutableList<Fragment> by lazy {
-        return@lazy mutableListOf<Fragment>().apply {
-            add(ClueNeedFragment.newInstance("线索"))
-            add(ClueNeedFragment.newInstance("需求"))
-            add(ClueNeedFragment.newInstance("商城"))
-            add(ClueNeedFragment.newInstance("我的"))
-            //            add(ClueNeedFragment.newInstance("你的"))
-        }
-    }
-
+    private val mFragmentList: MutableList<Fragment> = mutableListOf()
     private val mTitleArray: Array<String> by lazy {
-        return@lazy arrayOf("线索", "需求", "商城", "我的")
+        return@lazy arrayOf("线索", "需求1111111", "商城", "我", "我的2", "我的3")
     }
 
     override fun initData(savedInstanceState: Bundle?) {
-        val adapter = BaseViewPager2FragmentAdapter(this.supportFragmentManager, this.lifecycle, mFragmentList)
-        mBinding.vp.adapter = adapter
+        for (element in mTitleArray) {
+            mFragmentList.add(ClueNeedFragment.newInstance(element))
+        }
 
-        mBinding.tb.setItemBackgroundColor(Color.YELLOW).setItemSize(50F).withViewPager2(mBinding.vp, mTitleArray)
+//        // 2：滑动方式
+        mBinding.tbLayout.tabMode = TabLayout.MODE_AUTO // tablelayout的滑动方式，这里只有两个，所以设置为固定模式
+        val adapter = BaseViewPager2FragmentAdapter(supportFragmentManager, this.lifecycle, mFragmentList)
+        TabLayoutUtil2.Build()
+            .setTabLayout(mBinding.tbLayout)
+            .setViewPager2(mBinding.vp)
+            .setAdapter(adapter)
+            .setTitleList(mTitleArray.toMutableList())
+            .build()
+            .withViewPager2()
+            .setIndicator(this)
+//            .setIndicatorRatio(1f)
+            .isIndicatorFullWidth(false)
+//            .setIndicatorOffsetX(100)
+            .addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    setWidth()
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                }
+            })
+
+        mBinding.tb.setItemBackgroundColor(Color.YELLOW)
+            .setItemSize(50F)
+            .withViewPager2(mBinding.vp, mTitleArray)
     }
 
     override fun getBinding(
@@ -43,5 +65,14 @@ class CustomTabLayoutActivity : BaseBindingTitleActivity<ActivityCustomTabLayout
 
     override fun getTitleContent(): String {
         return "TabLayout"
+    }
+
+    fun setWidth() {
+        val selectedTabPosition = mBinding.tbLayout.selectedTabPosition
+
+//        val tabSelectedIndicator = mBinding.tbLayout.tabSelectedIndicator
+//        if (tabSelectedIndicator is LayerDrawable) {
+//            tabSelectedIndicator.setLayerInsetLeft(0, -50)
+//        }
     }
 }
