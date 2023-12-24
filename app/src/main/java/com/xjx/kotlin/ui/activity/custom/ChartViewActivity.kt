95a9e17945041e9d5d8a3899f10b36b6c3162074
xjxliuxing@163.com <xjxliuxing@163.com>
2023-12-24 13:32:25 +0800

    add animator test file

--- app/src/main/java/com/xjx/kotlin/ui/activity/custom/ChartViewActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/custom/ChartViewActivity.kt
@@ -9,26 +9,22 @@ import com.xjx.kotlin.databinding.ActivityChartViewBinding
 
 class ChartViewActivity : BaseBindingTitleActivity<ActivityChartViewBinding>() {
 
-    override fun getTitleContent(): String {
-        return "图表view"
-    }
+	override fun getTitleContent(): String {
+		return "图表view"
+	}
 
-    override fun getBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        attachToRoot: Boolean,
-    ): ActivityChartViewBinding {
-        return ActivityChartViewBinding.inflate(inflater, container, true)
-    }
+	override fun getBinding(inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean): ActivityChartViewBinding {
+		return ActivityChartViewBinding.inflate(inflater, container, attachToRoot)
+	}
 
-    override fun initData(savedInstanceState: Bundle?) {
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+	override fun initData(savedInstanceState: Bundle?) {
+		window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        mBinding.btnChart.setOnClickListener {
-            val bottomArray = floatArrayOf(0.6f, 0.3f, 0.2f, 0.1f, 0.4f)
-            val topArray = floatArrayOf(1f, 0.3f, 0.5f, 0.7f, 0.6f)
-            val scoreArray = intArrayOf(20, 30, 50, 20, 83)
-            mBinding.cvChart.setChartArray(bottomArray, topArray, scoreArray)
-        }
-    }
+		mBinding.btnChart.setOnClickListener {
+			val bottomArray = floatArrayOf(0.6f, 0.3f, 0.2f, 0.1f, 0.4f)
+			val topArray = floatArrayOf(1f, 0.3f, 0.5f, 0.7f, 0.6f)
+			val scoreArray = intArrayOf(20, 30, 50, 20, 83)
+			mBinding.cvChart.setChartArray(bottomArray, topArray, scoreArray)
+		}
+	}
 }
