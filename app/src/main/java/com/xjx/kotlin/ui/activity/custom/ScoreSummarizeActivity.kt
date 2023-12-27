88c75a0783ffa77eb9cb15eed40c5a6c846dc6b9
xjxliuxing@163.com <xjxliuxing@163.com>
2023-12-28 00:20:39 +0800

    add customView

--- app/src/main/java/com/xjx/kotlin/ui/activity/custom/ScoreSummarizeActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/custom/ScoreSummarizeActivity.kt
@@ -8,43 +8,41 @@ import com.android.common.interfaces.AnimationListener
 import com.xjx.kotlin.databinding.ActivityScoreSummarizeBinding
 
 class ScoreSummarizeActivity : BaseBindingTitleActivity<ActivityScoreSummarizeBinding>() {
+    override fun getTitleContent(): String {
+        return "分数综合"
+    }
 
-	override fun getTitleContent(): String {
-		return "分数综合"
-	}
+    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?,
+        attachToRoot: Boolean): ActivityScoreSummarizeBinding {
+        return ActivityScoreSummarizeBinding.inflate(inflater, container, true)
+    }
 
-	override fun getBinding(inflater: LayoutInflater, container: ViewGroup?,
-		attachToRoot: Boolean): ActivityScoreSummarizeBinding {
-		return ActivityScoreSummarizeBinding.inflate(inflater, container, true)
-	}
+    override fun initData(savedInstanceState: Bundle?) {
+        mBinding.spvScore.init(this, 10)
+        mBinding.cvChart.init(this)
+        mBinding.twvWrap.init(this)
 
-	override fun initData(savedInstanceState: Bundle?) {
-		mBinding.spvScore.init(this, 40)
-		mBinding.cvChart.init(this)
-		mBinding.twvWrap.init(this)
+        mBinding.btnStart.setOnClickListener {
+            mBinding.spvScore.reset()
+            mBinding.twvWrap.reset()
+            val bottomArray = floatArrayOf(0.6f, 0.3f, 0.2f, 0.1f, 0.4f)
+            val topArray = floatArrayOf(1f, 0.3f, 0.5f, 0.7f, 1f)
+            val scoreArray = intArrayOf(20, 30, 50, 20, 100)
+            mBinding.cvChart.setChartArray(bottomArray, topArray, scoreArray)
 
-		mBinding.btnStart.setOnClickListener {
-			mBinding.spvScore.reset()
-			mBinding.twvWrap.reset()
+            mBinding.cvChart.setAnimationListener(object : AnimationListener {
+                override fun onEndAnimation() {
+                    mBinding.spvScore.setScore(13, 41)
+                    mBinding.spvScore.setAnimationListener(object : AnimationListener {
+                        override fun onEndAnimation() {
+                            mBinding.twvWrap.setExplain("加油",
+                                "情绪稳定，心率不错，注意呼吸节奏，呼气更缓慢持久、吸气更平稳绵长、呼吸转换时适当憋气效果更好哦！吸转换时适当憋气效果更好哦！")
+                        }
+                    })
+                }
+            })
 
-			val bottomArray = floatArrayOf(0.6f, 0.3f, 0.2f, 0.1f, 0.4f)
-			val topArray = floatArrayOf(1f, 0.3f, 0.5f, 0.7f, 1f)
-			val scoreArray = intArrayOf(20, 30, 50, 20, 100)
-			mBinding.cvChart.setChartArray(bottomArray, topArray, scoreArray)
-
-			mBinding.cvChart.setAnimationListener(object : AnimationListener {
-				override fun onEndAnimation() {
-					mBinding.spvScore.setScore(93, 41)
-					mBinding.spvScore.setAnimationListener(object : AnimationListener {
-						override fun onEndAnimation() {
-							mBinding.twvWrap.setExplain("加油",
-								"情绪稳定，心率不错，注意呼吸节奏，呼气更缓慢持久、吸气更平稳绵长、呼吸转换时适当憋气效果更好哦！吸转换时适当憋气效果更好哦！")
-						}
-					})
-				}
-			})
-
-			this.getExternalFilesDir("")
-		}
-	}
+            this.getExternalFilesDir("")
+        }
+    }
 }
