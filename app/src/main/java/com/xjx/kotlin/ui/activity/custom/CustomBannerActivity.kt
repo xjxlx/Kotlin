173ec386d621d1e590c8fe51c10cd3d26275678c
xjxliuxing@163.com <xjxliuxing@163.com>
2024-01-07 19:11:52 +0800

    add customView

--- app/src/main/java/com/xjx/kotlin/ui/activity/custom/CustomBannerActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/custom/CustomBannerActivity.kt
@@ -3,28 +3,39 @@ package com.xjx.kotlin.ui.activity.custom
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.lifecycle.lifecycleScope
 import com.android.common.base.BaseBindingTitleActivity
 import com.xjx.kotlin.databinding.ActivityCustomBannerBinding
 import com.xjx.kotlin.databinding.CustomBannerBinding
 
 class CustomBannerActivity : BaseBindingTitleActivity<ActivityCustomBannerBinding>() {
+
     private val list: MutableList<String> = mutableListOf()
     private val mAdapter: BannerAdapter = BannerAdapter()
     override fun getTitleContent(): String {
         return "自定义Banner"
     }
 
-    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean): ActivityCustomBannerBinding {
+    override fun getBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        attachToRoot: Boolean
+    ): ActivityCustomBannerBinding {
         return ActivityCustomBannerBinding.inflate(inflater, container, attachToRoot)
     }
 
     override fun initData(savedInstanceState: Bundle?) {
-        for (index in 0..5) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        for (index in 0..4) {
             list.add("$index")
         }
         mAdapter.setList(list)
         mBinding.bv.setAdapter(mAdapter).setInterval(3000).withLifecycle(lifecycleScope).start()
+
+        mBinding.itv.setAdapter(mAdapter)
+        mBinding.itv.registerViewPager(mBinding.bv.getViewPager2())
     }
 
     class BannerAdapter : com.android.common.widget.banner.BannerAdapter<String, VH>() {
