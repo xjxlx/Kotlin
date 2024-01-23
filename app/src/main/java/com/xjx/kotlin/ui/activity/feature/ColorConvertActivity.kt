eab92dbb8bfe80d884e3ef7e7428d05de4a3dbc2
xjxliuxing@163.com <xjxliuxing@163.com>
2024-01-23 19:32:20 +0800

    update utils

--- app/src/main/java/com/xjx/kotlin/ui/activity/feature/ColorConvertActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/feature/ColorConvertActivity.kt
@@ -1,12 +1,23 @@
 package com.xjx.kotlin.ui.activity.feature
 
+import android.animation.ValueAnimator
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.android.common.base.BaseBindingTitleActivity
+import com.android.common.utils.LogUtil
 import com.xjx.kotlin.databinding.ActivityColorConvertBinding
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.sample
+import kotlinx.coroutines.launch
 
 class ColorConvertActivity : BaseBindingTitleActivity<ActivityColorConvertBinding>() {
+
+    private val mFlow: MutableStateFlow<Int> by lazy {
+        return@lazy MutableStateFlow(0)
+    }
 
     override fun getTitleContent(): String {
         return "测试颜色转换"
@@ -17,5 +28,26 @@ class ColorConvertActivity : BaseBindingTitleActivity<ActivityColorConvertBindin
     }
 
     override fun initData(savedInstanceState: Bundle?) {
+        lifecycleScope.launch {
+            mFlow.sample(100).collect {
+                LogUtil.e("color-2", "color - 2 :${it}")
+                mBinding.vBg.setBackgroundColor(it)
+            }
+        }
+
+        mBinding.btnStart.setOnClickListener {
+            var temp = 0
+            val animator = ValueAnimator.ofArgb(Color.RED, Color.BLUE)
+            animator.duration = 1000
+            animator.addUpdateListener {
+                val value = it.animatedValue as Int
+                if (value != temp) {
+                    LogUtil.e("color-1", "color - 1:${value}")
+                    mFlow.tryEmit(value)
+                    temp = value
+                }
+            }
+            animator.start()
+        }
     }
 }
