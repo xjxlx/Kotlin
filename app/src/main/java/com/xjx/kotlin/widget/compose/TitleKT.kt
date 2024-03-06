292b8a921893ffd0531c3b08ff8c42662a08ecc5
xjxliuxing@163.com <xjxliuxing@163.com>
2024-03-06 15:01:48 +0800

    add title

--- app/src/main/java/com/xjx/kotlin/widget/compose/TitleKT.kt
+++ app/src/main/java/com/xjx/kotlin/widget/compose/TitleKT.kt
@@ -1,15 +1,17 @@
 package com.xjx.kotlin.widget.compose
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,45 +28,48 @@ import com.android.common.utils.ToastUtil
 @Preview
 @Composable
 fun Title() {
-    TopAppBar(title = {
-        Surface(color = Color.Cyan) {
-            Row {
-                // back icon
-                Row(
-                    modifier = Modifier
-                        .fillMaxHeight()
-//                    .background(Color.Blue)
-                        .width(30.dp)
-                        .clickable {
-                            ToastUtil.show("111")
-                        }, verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start
-                ) {
-                    Image(painter = painterResource(id = R.mipmap.icon_base_title_back), contentDescription = "back")
-                }
-
-                // title
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .weight(1F)
-                        .fillMaxHeight()
-//                    .background(Color.Red)
-                ) {
-                    Text(
-                        text = "compose", modifier = Modifier, color = Color.Red
-                    )
-                }
-
-                // right title ---》aa
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxHeight()
-                ) {
-                    Text(text = "right", minLines = 1, style = TextStyle(fontSize = 18.sp, color = Color.White))
-                }
-            }
+    Row(
+        modifier = Modifier
+            .background(Color.Black)
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        // back icon
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(50.dp)
+                .clickable {
+                    ToastUtil.show("1111")
+                }, verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(painter = painterResource(id = R.mipmap.icon_base_title_back), contentDescription = "", tint = Color.White)
         }
-    })
+
+        // title
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1F),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "compose", style = TextStyle(fontSize = 18.sp, color = Color.White)
+            )
+        }
+
+        // right title
+        Row(
+            modifier = Modifier.fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "ediText",
+                style = TextStyle(fontSize = 18.sp, color = Color.White),
+                modifier = Modifier.padding(5.dp, 0.dp, 5.dp, 0.dp)
+            )
+        }
+    }
 }
