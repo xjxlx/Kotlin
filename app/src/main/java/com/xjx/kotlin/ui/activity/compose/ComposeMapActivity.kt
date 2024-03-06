52b35f1b2613e48123762aa8856be31a997cae43
xjxliuxing@163.com <xjxliuxing@163.com>
2024-03-06 16:52:56 +0800

    update title

--- app/src/main/java/com/xjx/kotlin/ui/activity/compose/ComposeMapActivity.kt
+++ app/src/main/java/com/xjx/kotlin/ui/activity/compose/ComposeMapActivity.kt
@@ -3,35 +3,32 @@ package com.xjx.kotlin.ui.activity.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.android.common.utils.ToastUtil
+import androidx.compose.ui.unit.sp
 import com.xjx.kotlin.ui.activity.compose.ui.theme.KotlinTheme
 import com.xjx.kotlin.widget.compose.Title
+import com.xjx.kotlin.widget.compose.TitleParameter
 
 class ComposeMapActivity : ComponentActivity() {
-
-    //    override fun getTitleContent(): String {
-//        return "Compose集合"
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,41 +41,51 @@ class ComposeMapActivity : ComponentActivity() {
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Preview
     @Composable
     fun LinearLayout() {
-        Title()
         Column(
-            Modifier
-                .background(Color.Yellow)
+            modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
         ) {
             // title
-            TopAppBar(title = {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(0.dp)
-                        .background(Color.Blue)
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .background(Color.Magenta)
-                            .width(50.dp)
-                            .fillMaxHeight()
-                            .clickable { ToastUtil.show("111") },
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Image(
-                            painter = painterResource(id = com.android.common.R.mipmap.icon_base_title_back),
-                            contentDescription = ""
-                        )
-                    }
+            Title(TitleParameter("Compose集合", { finish() }))
+
+            val scrollState = rememberScrollState()
+            Column(
+                Modifier
+                    .background(Color.Yellow)
+                    .fillMaxWidth()
+                    .weight(1F)
+                    .verticalScroll(scrollState)
+            ) {
+                for (index in 0..20) {
+                    Items(title = "$index")
                 }
-            })
+            }
+        }
+    }
+
+    @Composable
+    fun Items(title: String) {
+        // 增加间距
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .background(Color.Blue),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = title,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                style = TextStyle(color = Color.White, fontSize = 20.sp),
+            )
         }
     }
 }
