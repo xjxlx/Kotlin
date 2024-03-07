b4e64839323f40d18dcec8adb3141ee8e0a0ff54
xjxliuxing@163.com <xjxliuxing@163.com>
2024-03-07 13:46:45 +0800

    update title

--- compose/src/main/java/com/test/compose/MainActivity.kt
+++ compose/src/main/java/com/test/compose/MainActivity.kt
@@ -3,14 +3,32 @@ package com.test.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.test.compose.ui.theme.KotlinTheme
+import com.test.compose.view.Title
+import com.test.compose.view.TitleParameter
 
 class MainActivity : ComponentActivity() {
 
@@ -20,23 +38,57 @@ class MainActivity : ComponentActivity() {
             KotlinTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    LinearLayout()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-//    Text(text = "sss")
-}
+    @Preview
+    @Composable
+    fun LinearLayout() {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            // title
+            Title(TitleParameter("Compose集合", { finish() }))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinTheme {
-        Greeting("Android")
+            val scrollState = rememberScrollState()
+            Column(
+                Modifier
+                    .background(Color.Yellow)
+                    .fillMaxWidth()
+                    .weight(1F)
+                    .verticalScroll(scrollState)
+            ) {
+                for (index in 0..20) {
+                    Items(title = "$index")
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Items(title: String) {
+        // 增加间距
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .background(Color.Blue),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = title,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                style = TextStyle(color = Color.White, fontSize = 20.sp),
+            )
+        }
     }
 }
