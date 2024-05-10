072963b869c95a5b23415e6ae27e2426cfd3eee5
xjxliuxing <xjxliuxing@163.com>
2024-05-10 22:26:59 +0800

    add config

---
+++ app/src/main/java/com/xjx/kotlin/utils/hcp3/BaseRSIValue.java
@@ -0,0 +1,10 @@
+package com.xjx.kotlin.utils.hcp3;
+
+
+public abstract class BaseRSIValue {
+  //  protected BaseRSIValue(@NonNull XObject object) {
+  //    this.name = object.getName();
+  //    this.uri = object.getUri();
+  //    this.id = object.getId();
+  //  }
+}
--- app/src/main/java/com/xjx/kotlin/utils/hcp3/WriteSDK.java
+++ app/src/main/java/com/xjx/kotlin/utils/hcp3/WriteSDK.java
@@ -7,8 +7,9 @@ import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.annotation.Annotation;
 
 public class WriteSDK {
-  private static final String BASE_PATH = "src/main/java/com/xjx/kotlin/utils/hcp3/jar/";
-  private static final String TARGET_JAR_PATH = BASE_PATH + "mib_rsi_android.jar";
+  private static final String LIBS_FOLDER_PATH = "src/main/java/com/xjx/kotlin/utils/hcp3/jar/";
+  private static final String TARGET_JAR_PATH = LIBS_FOLDER_PATH + "mib_rsi_android.jar";
+  private static final String ENTITY_SUPERCLASS_PATH = "com.xjx.kotlin.utils.hcp3.BaseRSIValue";
 
   /**
    * @param targetFolderPath 文件写入成功后存放的位置，例如："./src/test"，注意，此处路径是以当前mode作为父节点的
@@ -20,22 +21,20 @@ public class WriteSDK {
       String targetFolderPath, HashMap<String, String> argumentsMap, String JarObjectPath) {
     try {
       ClassPool pool = ClassPool.getDefault();
-      // 1：添加jar文件到类路径
+      // 1：添加jar文件到类路径中，避免找不到类报错
       pool.appendClassPath(TARGET_JAR_PATH);
-      //      获取最后一个点号的索引位置
-      int lastIndex = JarObjectPath.lastIndexOf(".");
-      //    截取最后一个点号之后的字符串
-      String objectName = JarObjectPath.substring(lastIndex + 1);
       // 2：构建一个类对象
+      String objectName = JarObjectPath.substring(JarObjectPath.lastIndexOf(".") + 1);
       CtClass cls = pool.makeClass(objectName + "Entity");
+      // 3：循环写入成员变量
       for (Map.Entry<String, String> entry : argumentsMap.entrySet()) {
-        // 3：设置变量的类型
+        // 3.1：获取变量的类型
         CtClass type = pool.get(entry.getValue());
-        // 4：构建变量对象
+        // 3.2：构建变量对象
         CtField field = new CtField(type, entry.getKey(), cls);
-        // 5：设置字段的访问修饰符为 private final
+        // 3.3：设置字段的访问修饰符为 private final
         field.setModifiers(Modifier.PRIVATE | Modifier.FINAL);
-        // 6：给字段添加注解@Nullable
+        // 3.4：给字段添加注解@Nullable
         AnnotationsAttribute attr =
             new AnnotationsAttribute(
                 cls.getClassFile().getConstPool(), AnnotationsAttribute.visibleTag);
@@ -43,19 +42,15 @@ public class WriteSDK {
             new Annotation("javax.annotation.Nullable", cls.getClassFile().getConstPool());
         attr.setAnnotation(nullableAnnotation);
         field.getFieldInfo().addAttribute(attr);
-
-        // 7：在类中添加这个字段
+        // 3.5：在类中添加这个字段
         cls.addField(field);
       }
 
-      // cc.writeFile("/Users/XJX/Wokrs/StudioWorks/MY/Kotlin/app/src/test/java/com/xjx/kotlin");
-
-      // 8:写入构造方法
-
-      // 创建一个参数类型为String的构造方法
+      // 4：添加构造方法
+      // 4。1 获取构造方法的参数独享
       CtClass[] paramTypes = {pool.get(JarObjectPath)};
-      // 添加父类
-      cls.setSuperclass(pool.get(JarObjectPath));
+      // 4.2：添加类的父类对象
+      cls.setSuperclass(pool.get(ENTITY_SUPERCLASS_PATH));
       CtConstructor constructor = CtNewConstructor.make(paramTypes, null, cls);
       // 设置构造方法的修饰符为protected
       constructor.setModifiers(Modifier.PROTECTED);
@@ -66,8 +61,6 @@ public class WriteSDK {
 
       // 9：保存这个对象到文件中
       cls.writeFile(targetFolderPath);
-      System.out.println("ok");
-
       System.out.println("Custom type field added successfully.");
       // 清理
       cls.detach();
