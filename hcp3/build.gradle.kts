053e91fda0ddd1ee1de09226c5596d41faba4cd7
xjxliuxing@163.com <xjxliuxing@163.com>
2024-05-16 15:54:56 +0800

    add IGNORE class type

--- hcp3/build.gradle.kts
+++ hcp3/build.gradle.kts
@@ -11,6 +11,5 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     // 用于生成代码的依赖
     implementation("com.squareup:javapoet:1.13.0")
-    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
     implementation("org.projectlombok:lombok:1.18.30")
 }
