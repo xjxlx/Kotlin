7026ae5ee346306e24c1ea3cc9063032b7a8f387
xjxliuxing@163.com <xjxliuxing@163.com>
2024-05-17 10:48:52 +0800

    add IGNORE class type

---
+++ hcp3/src/main/java/com/android/hcp3/Test.java
@@ -0,0 +1,8 @@
+package com.android.hcp3;
+
+public class Test {
+  public static void main(String[] args) {
+    FileUtil.changePackage(
+        "hcp3/src/main/java/com/android/hcp3/TestFile.java", "com.xjx.android.helper.hvac");
+  }
+}
