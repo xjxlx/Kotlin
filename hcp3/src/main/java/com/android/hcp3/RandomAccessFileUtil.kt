024e9b9f5b64306441e24a71883baeb0b2ecead4
xjxliuxing@163.com <xjxliuxing@163.com>
2024-05-18 01:03:45 +0800

    add IGNORE class type

---
+++ hcp3/src/main/java/com/android/hcp3/RandomAccessFileUtil.kt
@@ -0,0 +1,61 @@
+package com.android.hcp3
+
+import java.io.IOException
+import java.io.RandomAccessFile
+
+object RandomAccessFileUtil {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val path = "hcp3/src/main/java/com/android/hcp3/TestFile.java"
+        randomAccess(path, "", "")
+    }
+
+    @JvmStatic
+    fun randomAccess(
+        filePath: String,
+        tag: String,
+        changContent: String,
+    ): Boolean {
+        try {
+            val random = RandomAccessFile(filePath, "rw")
+            var readLine: String? = null
+            var changePosition: Long = 0
+            while ((random.readLine().also { readLine = it }) != null) {
+                readLine?.let { line ->
+                    println("line:$line   changePosition:$changePosition")
+                    if (line.startsWith("package ")) {
+                        val length = random.length()
+                        val content = "package com.android.helper.xjx.test;"
+
+                        // 1：先跳到指定的位置，开始读取文件剩余的内容
+                        random.seek(changePosition)
+                        // 2：设置一个指定长度的字节数组，长度 = 文件总长度 - 开始改变的位置
+                        val byteArray = ByteArray(length.toInt() - changePosition.toInt())
+                        // 3：读取这个字节数组，把剩余的内容放到字节数组中
+                        random.read(byteArray)
+                        // 得到剩余的内容
+                        val originalBuffer = String(byteArray)
+                        println("【originalBuffer】:$originalBuffer")
+
+                        // 4：把指针设置到从改变的位置
+                        random.seek(changePosition)
+                        random.write(content.toByteArray(charset = Charsets.UTF_8))
+
+                        // 删除原来的数据
+                        val replace = originalBuffer.replace(line, "")
+                        random.write(replace.toByteArray())
+                    }
+                    // 必须把这个放到读取的后面，这样才能从指定的位置插入
+                    changePosition = random.filePointer
+                }
+            }
+            random.close()
+            println("【Random】文件[$filePath]的[$tag]内容修改成功。")
+//            return true
+        } catch (e: IOException) {
+            println(e)
+            println("文件[$filePath]的[$tag]内容修改失败：$e")
+        }
+        return false
+    }
+}
