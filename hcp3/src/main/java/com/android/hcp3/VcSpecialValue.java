f4b57b119956a2eb6af935f934558ae2679ab0bc
xjxliuxing@163.com <xjxliuxing@163.com>
2024-05-19 00:20:08 +0800

    update file resource

---
+++ hcp3/src/main/java/com/android/hcp3/VcSpecialValue.java
@@ -0,0 +1,37 @@
+package com.android.hcp3;
+
+import de.esolutions.fw.rudi.viwi.service.hvac.v3.SpecialValue;
+
+public enum VcSpecialValue {
+  _UNKNOWN_ENUM_VALUE("$UNKNOWN_ENUM_VALUE"),
+
+  ABSOLUTEDATE("absoluteDate"),
+
+  AUTO("auto"),
+
+  ECO("eco"),
+
+  HIGH("high"),
+
+  INFINITE("infinite"),
+
+  LOW("low"),
+
+  NEXTOCCURENCE("nextOccurence"),
+
+  OFF("off"),
+
+  ON("on"),
+
+  WEEKDAYS("weekDays");
+
+  private final String value;
+
+  VcSpecialValue(String object) {
+    this.value = object;
+  }
+
+  public static VcSpecialValue fromRSI(SpecialValue enumObject) {
+    return valueOf(enumObject.name());
+  }
+}
