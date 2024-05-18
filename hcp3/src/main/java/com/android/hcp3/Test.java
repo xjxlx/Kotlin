23c9de1f53230cd8ad79b117f6eb95e4791eac55
xjxliuxing@163.com <xjxliuxing@163.com>
2024-05-19 00:16:13 +0800

    update file util or string util

--- hcp3/src/main/java/com/android/hcp3/Test.java
+++ hcp3/src/main/java/com/android/hcp3/Test.java
@@ -2,7 +2,10 @@ package com.android.hcp3;
 
 public class Test {
   public static void main(String[] args) {
-    String path = "hcp3/src/main/java/com/android/hcp3/TestFile.java";
-    RandomAccessFileUtil.deleteFileContent(path, "// item = 3");
+    String path = "hcp3/src/main/java/com/android/hcp3/ValueControlObjectEntity.java";
+    RandomAccessFileUtil.changeFileContent(
+        path, "package com.android.hcp3;", "package com.android;");
+    //    import de.esolutions.fw.rudi.viwi.service.hvac.v3.ValueControlObject;
+
   }
 }
