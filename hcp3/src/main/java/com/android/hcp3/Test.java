466b4602409732835f7476b1ea0f4d92088c456d
xjxliuxing@163.com <xjxliuxing@163.com>
2024-05-19 16:33:38 +0800

    update file resource

--- hcp3/src/main/java/com/android/hcp3/Test.java
+++ hcp3/src/main/java/com/android/hcp3/Test.java
@@ -2,9 +2,9 @@ package com.android.hcp3;
 
 public class Test {
   public static void main(String[] args) {
-    String path = "hcp3/src/main/java/com/android/hcp3/VcSpecialValue.java";
+    String path = "hcp3/src/main/java/com/android/hcp3/TestFile.java";
     RandomAccessFileUtil.changeFileContent(
-        path, "package com.android.hcp3;", "package com.android.hcp4;");
+        path, "public class TestFile {", "public class TestFiles {");
     //        path, "package com.android.hcp3;", "package com.android;");
   }
 }
