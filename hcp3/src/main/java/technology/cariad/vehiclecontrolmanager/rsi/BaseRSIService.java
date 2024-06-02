55cb1ad6bc2ebd6f9590b0469eee76a595485817
xjxliuxing@163.com <xjxliuxing@163.com>
2024-06-03 00:14:24 +0800

    update ReadJarFile read file logic

--- hcp3/src/main/java/technology/cariad/vehiclecontrolmanager/rsi/BaseRSIService.java
+++ hcp3/src/main/java/technology/cariad/vehiclecontrolmanager/rsi/BaseRSIService.java
@@ -1,10 +1,17 @@
 package technology.cariad.vehiclecontrolmanager.rsi;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import de.esolutions.fw.android.rsi.client.rx.IRsiAdmin;
 import de.esolutions.fw.rudi.services.RsiService;
 
 public abstract class BaseRSIService<S extends RsiService> implements ServiceProvider<S> {
 
   protected BaseRSIService(@NonNull IRsiAdmin rsiAdmin) {}
+
+  @Nullable
+  @Override
+  public S getServiceBlocking() {
+    return null;
+  }
 }
