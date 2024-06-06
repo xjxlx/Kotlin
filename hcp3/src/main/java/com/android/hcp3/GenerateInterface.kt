e19085a0298bb0d6b6559e26dfd2bf26b44c1c81
xjxliuxing@163.com <xjxliuxing@163.com>
2024-06-07 01:12:19 +0800

    更新 file 逻辑！

--- hcp3/src/main/java/com/android/hcp3/GenerateInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.android.hcp3
-
-object GenerateInterface {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        ReadJarFile.readNeedDependenciesClassName()
-        // GenerateUtil.generateInterface()
-    }
-}
