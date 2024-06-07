2b427939183c14c5b46f1df32281896f97aea58d
xjxliuxing@163.com <xjxliuxing@163.com>
2024-06-07 23:35:47 +0800

    update enum entity

--- hcp3/src/main/java/com/android/hcp3/ClassTypeEnum.kt
+++ hcp3/src/main/java/com/android/hcp3/ClassTypeEnum.kt
@@ -1,27 +1,27 @@
-package com.android.hcp3;
+package com.android.hcp3
 
-public enum ClassTypeEnum {
-  /** 无效的数据类型 */
-  INVALID,
+enum class ClassTypeEnum {
+    /** 无效的数据类型  */
+    INVALID,
 
-  /** 基础数据类型 */
-  PRIMITIVE,
+    /** 基础数据类型  */
+    PRIMITIVE,
 
-  /** list集合，泛型是基础数据类型 */
-  LIST_PRIMITIVE,
+    /** list集合，泛型是基础数据类型  */
+    LIST_PRIMITIVE,
 
-  /** 自定义object类型数据 */
-  OBJECT,
+    /** 自定义object类型数据  */
+    OBJECT,
 
-  /** 枚举类型数据 */
-  ENUM,
+    /** 枚举类型数据  */
+    ENUM,
 
-  /** list集合，泛型是引用数据类型 */
-  LIST_OBJECT,
+    /** list集合，泛型是引用数据类型  */
+    LIST_OBJECT,
 
-  /** list集合，泛型是enum数据类型 */
-  LIST_ENUM,
+    /** list集合，泛型是enum数据类型  */
+    LIST_ENUM,
 
-  /** 数组类型数据，暂时不用 */
-  ARRAY
+    /** 数组类型数据，暂时不用  */
+    ARRAY,
 }
