ba067d8f860070da8192da76d69c0a3742c65a71
xjxliuxing@163.com <xjxliuxing@163.com>
2024-06-11 10:43:02 +0800

    add java model

--- java/src/main/java/com/java/test/ServiceSocket.kt
+++ java/src/main/java/com/java/test/ServiceSocket.kt
@@ -7,12 +7,11 @@ import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.ScheduledThreadPoolExecutor
 import java.util.concurrent.TimeUnit
-import java.util.logging.Logger
 
 object ServiceSocket {
     private var mSocket: Socket? = null
     private var mScheduledFuture: ScheduledFuture<*>? = null
-    private val mLogger: Logger = Logger.getLogger(SocketTest::class.java.name)
+//    private val mLogger: Logger = Logger.getLogger(SocketTest::class.java.name)
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -24,8 +23,9 @@ object ServiceSocket {
         val mRunnable =
             Runnable {
                 if ((mSocket == null) || (!mSocket!!.isConnected)) {
-                    mSocket = Socket("localhost", 18000)
+                    mSocket = Socket("127.0.0.1", 66666)
                     println("建立Socket的service端！")
+                    mSocket
                     CompletableFuture.runAsync { session() }
                 }
             }
@@ -45,10 +45,8 @@ object ServiceSocket {
                 val data = "PC时间:" + System.currentTimeMillis()
                 dos.writeUTF(data)
                 dos.flush()
-
                 val s = dis.readUTF()
-                mLogger.info("收到数据:$s")
-
+                println("收到数据:$s")
                 Thread.sleep(5000)
             }
         } catch (e: Exception) {
